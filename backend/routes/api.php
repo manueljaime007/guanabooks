7a81370eb046e16a89981abb876a56747283ab2b
Manuel Jaime <manueljaime0020@gmail.com>
2026-06-02 01:57:13 +0100

    fix: update backend route files

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,5 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+    // return $request->user();
+    return $request->json([
+        'message' => 'Olá, Guana!'
+    ]);
 })->middleware('auth:sanctum');
+
+Route::get('/', function (Request $request) {
+    return ([
+        'message' => 'Olá, Guana!'
+    ]);
+});
