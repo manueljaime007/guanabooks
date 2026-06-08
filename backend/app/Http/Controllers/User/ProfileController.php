e52c2ea855a3c4eae6d231e5c25fddc7a52ca553
Manuel Jaime <manueljaime0020@gmail.com>
2026-06-08 16:34:31 +0100

    feat:add user controllers with api methods

---
+++ backend/app/Http/Controllers/User/ProfileController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+
+    public function profile()
+    {
+        //
+    }
+
+    public function stats()
+    {
+        //
+    }
+
+    public function updateProfile(Request $request, User $user)
+    {
+        //
+    }
+}
